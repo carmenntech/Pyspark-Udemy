71ae1f185ff1a4edb4727dec13fa44df4ed1111d
carmenpatit <carmentorrecilla6@gmail.com>
2025-01-28 14:35:45 +0100

    Ejercicio 1

--- Ejercicios/01-Ejercicios-RDDs/Ejercicio-3-rdd.py
+++ Ejercicios/01-Ejercicios-RDDs/Ejercicio-3-rdd.py
@@ -9,31 +9,23 @@ spark = SparkSession.builder.appName("Ejercicio_Ciudades").getOrCreate()
 
 lines = spark.sparkContext.textFile("dbfs:/FileStore/tables/perfilesmongo-1.csv")
 
-#results = lines.collect()
-#print(results)
-
+#Divisiones por ; y |
 rdd_ciudades = lines.map(lambda x: (x.split(";")[1].strip()))
+rdd_ciudades1 = rdd_ciudades_sin_header.flatMap(lambda x: (x.split("|")))
 
-header = rdd_ciudades.first()
-rdd_ciudades_sin_header = rdd_ciudades.filter(lambda x: x not in [header, '"'])
+#Filtros
+header = rdd_ciudades1.first()
+rdd_ciudades_sin_header = rdd_ciudades1.filter(lambda x: x != header and '"' not in x and x.strip() != "")
 
-rdd_ciudades1 = rdd_ciudades_sin_header.flatMap(lambda x: (x.split("| ")))
-rdd_ciudades2 = rdd_ciudades1.map(lambda x: (x.strip()))
-rdd_ciudades3 = rdd_ciudades2.filter(lambda x: x != '"')
-rdd_ciudades4 = rdd_ciudades3.map(lambda x: (x , 1))
-
-#results = rdd_ciudades3.collect()
-#print(results)
-
-#for element in rdd_ciudades.collect():
-#    print(element)
-
+#Poner 1 y sumar
+rdd_ciudades4 = rdd_ciudades_sin_header.map(lambda x: (x , 1))
 ciudadesTotal = rdd_ciudades4.reduceByKey(lambda a,b: a+b)
 
-skills_ordenados = ciudadesTotal.sortBy(lambda x: x[1]).collect()
+#Ordenar
+skills_ordenados = ciudadesTotal.sortBy(lambda x: x[1])
 
-
-for element in skills_ordenados:
+#Mostrar
+for element in skills_ordenados.collect():
     print(element)
 
 
