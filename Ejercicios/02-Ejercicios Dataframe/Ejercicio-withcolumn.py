e604a1e737eef38a44f71ec3246b7d79a2419829
carmenpatit <carmentorrecilla6@gmail.com>
2025-02-06 10:02:25 +0100

    Ejercicio withcolumn

--- Ejercicios/02-Ejercicios Dataframe/Ejercicio-withcolumn.py
+++ Ejercicios/02-Ejercicios Dataframe/Ejercicio-withcolumn.py
@@ -1,40 +1,31 @@
 # Databricks notebook source
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import trim, when, col
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql.types import StructField, StructType, StringType, IntegerType
 
-# Crear una sesión de Spark
-spark = SparkSession.builder.appName("Ejercicio_Ciudades").getOrCreate()
+spark = SparkSession.builder.appName("Ejercicio_withCOlumn").getOrCreate()
 
-df_inferido = spark.read.option("header", True).option("sep",";").option("inferSchema", True).csv("dbfs:/FileStore/tables/perfilesmongo.csv")
+df_inferido = spark.read.option("header", True).option("sep", ";").option("inferSchema", True).csv("dbfs:/FileStore/tables/perfilesmongo.csv")
 
-df_inferido.printSchema()
-
-df_inferido.show()
+#df_inferido.show()
+#df_inferido.printSchema()
 
 esquema = StructType([
-    StructField("_id", StringType(), False),
+
+    StructField("_id", StringType(),False),
     StructField("skills", StringType(), False),
     StructField("location", StringType(), False),
     StructField("numcontactos", StringType(), False)
 ])
 
-df_esquema_creado = spark.read.option("header", True).schema(esquema).csv("dbfs:/FileStore/tables/perfilesmongo.csv")
-df_esquema_creado.printSchema()
+df_esquema_creado = spark.read.option("header", True).option("sep",";").schema(esquema).csv("dbfs:/FileStore/tables/perfilesmongo.csv")
 
-df_esquema_creado1 = df_esquema_creado.withColumn("numcontactos", df_esquema_creado.numcontactos.cast(IntegerType()))
-df_esquema_creado1.printSchema()
+#df_esquema_creado.show()
+#df_esquema_creado.printSchema()
 
+df_esquema_creado = df_esquema_creado.withColumn("numcontactos", df_esquema_creado.numcontactos.cast(IntegerType()))
 
+#df_esquema_creado.printSchema()
 
-df_inferido.withColumn("numcontactos2", (df_inferido.numcontactos + 10)).show()
+df_inferido1 = df_inferido.withColumn("numcontactos2", (df_inferido.numcontactos + 10)).show()
 
-df_inferido.withColumn("newcontactos2", (df_inferido.numcontactos - 10)).show()
-
-df_inferido.withColumnRenamed("numcontactos", "numcontactos1").show()
-
-
-
-
-
-
+df_esquema_creado.withColumnRenamed("numcontactos", "numcontactos1").show()
